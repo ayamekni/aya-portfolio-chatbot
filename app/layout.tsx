7dbc7b7abf6cdad42e96809e61aeb40c7db19034
ayamekni <aya.mekni10@gmail.com>
2025-11-15 02:54:02 +0100

    Fix the background

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,19 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body style={{ margin: 0, background: '#0e2b31', color: 'white', fontFamily: 'ui-sans-serif, system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, Helvetica Neue, Arial' }}>
-				{children}
-			</body>
+			<body
+  style={{
+    margin: 0,
+    background: 'rgba(14, 18, 40, 0.3)', // semi-transparent navy
+    backdropFilter: 'blur(10px)',
+    color: 'white',
+    fontFamily:
+      'ui-sans-serif, system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, Helvetica Neue, Arial',
+  }}
+>
+  {children}
+</body>
+
 		</html>
 	);
 }
